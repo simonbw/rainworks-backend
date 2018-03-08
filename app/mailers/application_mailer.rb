ae8da3396d352315578105ecf998e0a0cecec65d
simon <simonbw@uw.edu>
2018-03-07 21:35:35 -0800

    Mailer improvements

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'notifications@rain.works'
+  default from: ENV['NOTIFICATIONS_FROM_EMAIL']
   layout 'mailer'
 end
--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,13 +1,12 @@
 class NotificationsMailer < ApplicationMailer
-	default from: 'notifications@rain.works'
-
+	default to: ENV['NOTIFICATIONS_TO_EMAIL']
 	def submission_alert(rainwork)
 		@rainwork = rainwork
-		mail to: "thecheeseinator@gmail.com", subject: "Test Notification"
+		mail subject: "Someone submitted a rainwork"
 	end
 
 	def report_alert(report)
 		@report = report
-		mail to: "thecheeseinator@gmail.com", subject: "Test Notification"
+		mail subject: "Someone reported a rainwork: #{@report.report_type}"
 	end
 end
