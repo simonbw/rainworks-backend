41f98d879833fb77c839114df598a8e1a072e341
Voke okpeka <vefe44@gmail.com>
2021-11-17 06:04:00 +0100

    update devices dashboard

--- app/dashboards/device_dashboard.rb
+++ app/dashboards/device_dashboard.rb
@@ -8,15 +8,15 @@ class DeviceDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    rainworks: Field::HasMany,
-    reports: Field::HasMany,
     id: Field::Number,
     device_uuid: Field::String,
+    rainworks: Field::HasMany,
+    reports: Field::HasMany,
     push_token: Field::String,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
-    nickname: Field::String,
     initial_submission_status: Field::Select.with_options(searchable: false, collection: ->(field) { field.resource.class.send(field.attribute.to_s.pluralize).keys }),
+    nickname: Field::String,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -25,36 +25,34 @@ class DeviceDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
+    nickname
+    device_uuid
     rainworks
     reports
-    id
-    device_uuid
+    created_at
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-    rainworks
-    reports
-    id
+    nickname
+    initial_submission_status
     device_uuid
     push_token
     created_at
     updated_at
-    nickname
-    initial_submission_status
+    reports
+    rainworks
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    rainworks
-    reports
-    device_uuid
-    push_token
     nickname
     initial_submission_status
+    device_uuid
+    push_token
   ].freeze
 
   # COLLECTION_FILTERS
@@ -72,7 +70,10 @@ class DeviceDashboard < Administrate::BaseDashboard
   # Overwrite this method to customize how devices are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(device)
-  #   "Device ##{device.id}"
-  # end
+  def display_resource(device)
+    if device.nickname
+      return "#{device.nickname} (#{device.device_uuid})"
+    end
+    "Device #(#{device.device_uuid})"
+  end
 end
