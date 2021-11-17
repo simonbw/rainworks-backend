7bf329d6ab16564aea57f176b0a2708a95d65874
Voke okpeka <vefe44@gmail.com>
2021-11-17 04:44:02 +0100

    custom application controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -10,12 +10,26 @@ module Admin
 
     def authenticate_admin
       # TODO Add authentication logic here.
+      http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
+    end
+
+    # sort list by created_at date with the most recent at the top
+    def order
+      @order ||= Administrate::Order.new(
+        params.fetch(resource_name, {}).fetch(:order, 'created_at'),
+        params.fetch(resource_name, {}).fetch(:direction, 'desc'),
+      )
+    end
+
+    # override this in specific controllers as needed
+    def default_sort
+      { order: :updated_at, direction: :desc }
     end
 
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
-    # def records_per_page
-    #   params[:per_page] || 20
-    # end
+    def records_per_page
+      params[:per_page] || 50
+    end
   end
 end
