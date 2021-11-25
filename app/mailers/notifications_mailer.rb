ade5782f9a2cef0c83dc8b23c175318c73e31f5e
Voke okpeka <vefe44@gmail.com>
2021-11-26 00:08:21 +0100

    use api key for mail

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -2,7 +2,7 @@ class NotificationsMailer < ApplicationMailer
 	default to: ENV['NOTIFICATIONS_TO_EMAIL']
 	def submission_alert(rainwork)
 		@rainwork = rainwork
-		mail subject: "Someone submitted a rainwork"
+		mail subject: 'Someone submitted a rainwork'
 	end
 
 	def report_alert(report)
