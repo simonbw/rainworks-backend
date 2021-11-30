2dd9e5fa859c7ba43c36ec99801373aeab1f1aa8
Voke okpeka <vefe44@gmail.com>
2021-11-30 05:00:28 +0100

    sendgrid fix

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,8 +1,26 @@
+require 'sendgrid-ruby'
+include SendGrid
+require 'json'
+
 class NotificationsMailer < ApplicationMailer
 	default :from => ENV['NOTIFICATIONS_FROM_EMAIL']
+
+	
+
 	def submission_alert(rainwork)
 		@rainwork = rainwork
-		mail :to => ENV['NOTIFICATIONS_TO_EMAIL'], :subject=> 'Someone submitted a rainwork'
+		# mail :to => ENV['NOTIFICATIONS_TO_EMAIL'], :subject=> 'Someone submitted a rainwork'
+		from = Email.new(email: ENV['NOTIFICATIONS_FROM_EMAIL'])
+		subject = 'Someone submitted a rainwork'
+		to = Email.new(email: ENV['NOTIFICATIONS_TO_EMAIL'])
+		mail = SendGrid::Mail.new(from, subject, to, @rainwork)
+		puts mail.to_json
+	  
+		sg = SendGrid::API.new(api_key: ENV['SENDGRID_PASSWORD'])
+		response = sg.client.mail._('send').post(request_body: mail.to_json)
+		puts response.status_code
+		puts response.body
+		puts response.headers
 	end
 
 	def report_alert(report)
