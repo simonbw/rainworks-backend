ab5a95cd37f9d7502a4dc47601ddb4a38638071d
Voke okpeka <vefe44@gmail.com>
2022-01-11 00:00:02 +0100

    submission update api email alert

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,13 +1,14 @@
 class NotificationsMailer < ApplicationMailer
 	default :from => ENV['NOTIFICATIONS_FROM_EMAIL']
-	def submission_alert(rainwork)
-		@rainwork = rainwork
-		mail :to => ENV['NOTIFICATIONS_TO_EMAIL'], :subject=> 'Someone submitted a rainwork'
-	end
 
 	def edit_alert(rainwork)
 		@rainwork = rainwork
 		mail :to => ENV['NOTIFICATIONS_TO_EMAIL'], :subject=> 'Someone edited a rainwork'
+	end
+	
+	def submission_alert(rainwork)
+		@rainwork = rainwork
+		mail :to => ENV['NOTIFICATIONS_TO_EMAIL'], :subject=> 'Someone submitted a rainwork'
 	end
 
 	def report_alert(report)
