01e56dac29e3436e5233c9a4183bb6cf1e113054
Voke Okpeka <vefe44@gmail.com>
2023-01-29 20:26:15 +0100

    debug

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,9 +1,25 @@
+require 'sendgrid-ruby'
+include SendGrid
+
+
 class NotificationsMailer < ApplicationMailer
 	default :from => ENV['NOTIFICATIONS_FROM_EMAIL']
 
-	def submission_alert(rainwork)
+	def submissions_alert(rainwork)
 		@rainwork = rainwork
-		mail :to => ENV['NOTIFICATIONS_TO_EMAIL'], :subject=> 'Someone submitted a rainwork'
+		subject = 'Hello World from the Twilio SendGrid Ruby Library'
+		to = Email.new(email: ENV['NOTIFICATIONS_TO_EMAIL'])
+		content = Content.new(type: 'text/plain', value: 'some text here')
+		mail = SendGrid::Mail.new(from, subject, to, content)
+		puts JSON.pretty_generate(mail.to_json)
+		puts mail.to_json
+	
+		sg = SendGrid::API.new(api_key: ENV['SENDGRID_PASSWORD'])
+		response = sg.client.mail._('send').post(request_body: mail.to_json)
+		puts response.status_code
+		puts response.body
+		puts response.headers
+		# mail :to => ENV['NOTIFICATIONS_TO_EMAIL'], :subject=> 'Someone submitted a rainwork'
 	end
 
 	def edit_alert(rainwork)
